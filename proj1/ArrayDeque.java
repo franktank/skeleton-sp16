5e3680fe14fcdc74f1d5e356a1dbfb2ae2eeeb6e
Liang <fliangz@fp01-5258-m1.local>
2016-05-17 20:14:15 -0400

    built out more functions

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -2,20 +2,27 @@ public class ArrayDeque<Item> {
   private Item[] items;
   private int size;
 
+  //pointers of first and last?
+  private int nextFirst;
+  private int nextLast;
+
   private static int RFACTOR = 2;
 
   public ArrayDeque() {
-    size = 0;
+    size = 8;
     items = (Item[]) new Object[8];
   }
 
   public void addFirst(Item item) {
-
+    items[] = item;
+    nextFirst--;
+    size++;
   }
 
   public void addLast(Item item) {
     //Resize after basic implementation
     items[size] = item;
+    nextLast++;
     size++;
   }
 
@@ -32,11 +39,13 @@ public class ArrayDeque<Item> {
   }
 
   public void printDeque() {
-
+    for (int i = 0; i < items.length; i++) {
+      System.out.println("Item: " + [items[i]]);
+    }
   }
 
   public Item removeFirst() {
-    
+
   }
 
   public Item getBack() {
@@ -45,10 +54,14 @@ public class ArrayDeque<Item> {
   }
 
   public Item removeLast() {
-    Item itemToReturn = getBack();
-    items[size - 1] = null;
-    size--;
-    return itemToReturn;
+    if (isEmpty()) {
+      return null;
+    } else {
+      Item itemToReturn = getBack();
+      items[size - 1] = null;
+      size--;
+      return itemToReturn;
+    }
   }
 
   public Item get(int index) {
